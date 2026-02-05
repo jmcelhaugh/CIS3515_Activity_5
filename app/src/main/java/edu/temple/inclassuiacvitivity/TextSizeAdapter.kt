f435673cd7902c02788067dab4b45f31e05da907
jmcelhaugh <tup59389@temple.edu>
2026-02-05 12:15:04 -0500

    Adds custom adapter with the change in text size throughout the spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -3,23 +3,45 @@ package edu.temple.inclassuiacvitivity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
+import android.content.Context
 
-class TextSizeAdapter : BaseAdapter() {
+
+class TextSizeAdapter(_context: Context, _numbers: Array<Int>) : BaseAdapter() {
+
+    private val numbers = _numbers
+    private val context = _context
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+        return numbers.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+        return numbers[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = numbers[position].toString()
+        textView.textSize = numbers[position].toFloat()
+        textView.setPadding(5,10,0,5)
+        return textView
+
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        return super.getDropDownView(position, convertView, parent).apply{
+            //didn't fully finish this part, had a bug with code not being in context (aka put
+            //after the todo
+        }
     }
 
 }
